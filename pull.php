4118f8b39f2a44d0cdccfe3874418aec75570d72
Joe Adames <jadames14@gmail.com>
2023-10-18 15:09:59 -0400

    test

--- pull.php
+++ pull.php
@@ -1,5 +1,3 @@
-<?php
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-if ( $_POST['payload'] ) { 
-        shell_exec('cd ~/public_html && git pull');
-    }
+<?php// Use in the “Post-Receive URLs” section of your GitHub repo.
+$output = shell_exec('cd ~/public_html && git pull');
+echo $output;
