c45dc24f3964a6ada924f29ab159489ca07fa73f
Joe Adames <jadames14@gmail.com>
2023-10-18 15:15:59 -0400

    no more cuffs

--- pull.php
+++ pull.php
@@ -1,3 +1,2 @@
 <?php// Use in the “Post-Receive URLs” section of your GitHub repo.
-$output = shell_exec('cd ~/public_html && git pull');
-echo $output;
+shell_exec('cd ~/public_html && git pull');
